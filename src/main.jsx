3f5a439938446bfb20b056a903781425433fc2a4
shahina akter shima <shima421914@gmail.com>
2024-03-17 22:02:11 +0600

    data

--- src/main.jsx
+++ src/main.jsx
@@ -36,12 +36,12 @@ const router = createBrowserRouter([
     {
       path:'/details/:id',
       element:<Details></Details>,
-      loader:({params})=>fetch(`https://final-project-xi-eight.vercel.app/meals/${params.id}`)
+      loader:({params})=>fetch(`https://final-project-new.vercel.app/meals/${params.id}`)
     },
     {
 path:'/detail/:id',
 element:<Detail></Detail>,
-loader:({params})=>fetch(`https://final-project-xi-eight.vercel.app/addmeal/${params.id}`)
+loader:({params})=>fetch(`https://final-project-new.vercel.app/addmeal/${params.id}`)
     },
     {
       path:'/meals',
--- src/useHooks/useAxiosSecure.jsx
+++ src/useHooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure= axios.create({
-    baseURL:'https://final-project-xi-eight.vercel.app'
+    baseURL:'https://final-project-new.vercel.app'
 })
 const useAxiosSecure = () => {
     return axiosSecure;
