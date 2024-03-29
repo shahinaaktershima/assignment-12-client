a9a3a4456007525efc89ca81cfd80ec73ce5a691
shahina akter shima <shima421914@gmail.com>
2024-03-29 12:32:48 +0600

    data

--- src/usersetup/Login.jsx
+++ src/usersetup/Login.jsx
@@ -75,11 +75,11 @@ const Login = () => {
        
          <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex flex-col ">
-          <div className="text-center lg:text-left md:w-1/2">
+          <div className="text-center lg:text-left w-full md:max-w-md">
             <h1 className="text-5xl font-bold">Login now!</h1>
             
           </div>
-          <div className="card  md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+          <div className="card   shadow-2xl bg-base-100">
             <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
